03ce66716c6d324a96b12d4549202205a92e3805
Kuntal <banerjee.kuntal.1990.aws@gmail.com>
2024-05-09 18:13:29 +0530

    added raw react code

---
+++ ui/devops-assignment/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './components/Banner';
+import './index.css';
+const App = () => {
+  return (
+      <div className="App">
+        <Banner />
+      </div>
+  );
+};
+
+export default App;
---
+++ ui/devops-assignment/src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        // Full-height div for proper centering and background coverage
+        <div className="h-screen w-full flex justify-center items-center bg-gray-800 text-white">
+            <div
+                className="banner-bg w-full h-full bg-cover bg-center"
+            >
+                <div className="h-full w-full bg-black bg-opacity-50 flex flex-col justify-center items-center">
+                    <h1 className="heading-one text-4xl font-bold">Welcome to DevOps Assignment</h1>
+                    <p className="heading-two text-lg mt-4">Building and Deploying with DevOps Best Practices</p>
+                    <p className="heading-three">Developed By - Debjyoti Paul</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
